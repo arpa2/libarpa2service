c01b7d7e09da715db9a9116a98f8f940caa12a68
Tim Kuijsten <tim@sans.netsend.nl>
2019-07-12 16:58:47 +0200

    a2id.h: add introductionary comment

--- src/a2id.h
+++ src/a2id.h
@@ -14,6 +14,16 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
+/*
+ * Public API of the ARPA2 Identifier and ARPA2 Identifier Selector library.
+ *
+ * Input from a string and output to a string is utilized with a2id_fromstr(3)
+ * and a2id_tostr(3).
+ *
+ * Once a string is successfully converted into an A2ID using a2id_fromstr,
+ * the other utility functions that take an "a2id" can be used.
+ */
+
 #ifndef A2ID_H
 #define A2ID_H
 
@@ -21,21 +31,22 @@
 #include <stdint.h>
 
 /*
- * Practial upper bound to the lenght of an ARPA2 ID (not including an optional
- * terminating null.
+ * The maximum length of an A2ID is 512 bytes, excluding a terminating nul byte.
  */
 #define A2ID_MAXLEN 512
 
 enum A2ID_TYPE { A2IDT_DOMAINONLY, A2IDT_GENERIC, A2IDT_SERVICE };
 
+/* opaque id container */
 typedef struct {
 	uint8_t a2id[((A2ID_MAXLEN) + 128)];
 } a2id;
 
-int a2id_hassignature(const a2id *a2id);
+/* import from, and export to a nul terminated string */
 int a2id_fromstr(a2id *a2id, const char *in, int isselector);
 size_t a2id_tostr(char *dst, size_t dstsz, const a2id *a2id);
 
+int a2id_hassignature(const a2id *a2id);
 int a2id_coreform(char *dst, const a2id *a2id, size_t *dstsize);
 int a2id_generalize(a2id *a2id);
 int a2id_match(const a2id *subject, const a2id *selector);
